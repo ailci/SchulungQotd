a9a0891892bf9a71d85b13b757bb73932f11e6b2
Ali Ilci <Ali.Ilci@gmx.de>
2024-05-22 11:36:10 +0200

    Add project files.

---
+++ SchulungQotd.Mvc/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SchulungQotd.Mvc.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+//MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//Startbeispiel Middleware ############################################################################################
+//app.Use(async (context, next) =>
+//{
+//    var userAgent = context.Request.Headers["User-Agent"];
+//    await context.Response.WriteAsync($"First middleware {userAgent}\n");
+//    await next();
+//    await context.Response.WriteAsync("First middleware Rückweg\n");
+//});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Second middleware\n");
+//    await next();
+//});
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("End middleware\n");
+//});
+//Endbeispiel Middleware ##############################################################################################
+
+// Configure the HTTP request pipeline. ##############################################################################
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
