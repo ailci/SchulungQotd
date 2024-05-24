800f8d047f2fef2e4b1f9e4f2d9949c40fcf8e81
Ali Ilci <Ali.Ilci@gmx.de>
2024-05-24 14:46:09 +0200

    ViewComponents

---
+++ SchulungMvc.Common/ViewModels/QotdSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchulungMvc.Common.ViewModels
+{
+    public class QotdSummaryViewModel
+    {
+        public int AuthorsQuantity { get; set; }
+        public int QuotesQuantity { get; set; }
+    }
+}
---
+++ SchulungQotd.Mvc/ViewComponents/QotdSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchulungMvc.Common.ViewModels;
+using SchulungQotd.Service;
+
+namespace SchulungQotd.Mvc.ViewComponents
+{
+    public class QotdSummaryViewComponent(IQotdService qotdService) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var authors = (await qotdService.GetAuthorsAsync(includeQuotes: true)).ToList();
+
+            var qotsSummaryVm = new QotdSummaryViewModel
+            {
+                AuthorsQuantity = authors.Count,
+                QuotesQuantity = authors.Sum(c => c.Quotes.Count)
+            };
+
+            //return View(qotsSummaryVm);
+
+            return View("Alternative", qotsSummaryVm);
+        }
+    }
+}
