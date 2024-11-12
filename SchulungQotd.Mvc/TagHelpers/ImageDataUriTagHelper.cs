05bbc5d310156e02200490665ea94447cb5ce761
ailci <ali.ilci@gmx.de>
2024-11-12 16:21:12 +0100

    Update ImageDataUriTagHelper and refactor image rendering
    Updated ImageDataUriTagHelper to change AltSrc default value and
    modify Process method. Refactored Index.cshtml to use the new
    tag helper for image rendering. Added SchulungQotd.Mvc tag
    helpers to _ViewImports.cshtml.

--- SchulungQotd.Mvc/TagHelpers/ImageDataUriTagHelper.cs
+++ SchulungQotd.Mvc/TagHelpers/ImageDataUriTagHelper.cs
@@ -2,16 +2,26 @@
 
 namespace SchulungQotd.Mvc.TagHelpers
 {
+    // <imagedataurl....
     public class ImageDataUriTagHelper : TagHelper
     {
         public string? ImageMimeType { get; set; }
         public byte[]? Image { get; set; }
         public string Width { get; set; } = "120";
-        public string AltSrc { get; set; } = "https://via.placeholder.com/150";
+        public string AltSrc { get; set; } = "/images/noimg.jpg";
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            
+            output.TagName = "img";  // <img 
+            output.Attributes.SetAttribute("width", Width);  // <img width="150"
+
+            // <img src="Inhalt"
+            var src = !string.IsNullOrEmpty(ImageMimeType) && Image is not null && Image?.Length > 0
+                ? $"data:{ImageMimeType};base64,{Convert.ToBase64String(Image)}"
+                : AltSrc;
+
+            output.Attributes.SetAttribute("src", src);
+            output.TagMode = TagMode.SelfClosing;
         }
     }
 }
