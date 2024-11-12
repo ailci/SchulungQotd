b60bf86c70fa9b4582fa452bd2b86fc8fb9c9b02
ailci <ali.ilci@gmx.de>
2024-11-12 16:55:35 +0100

    Add EmailTagHelper for custom <kontakt> tag in Razor views
    Introduce a new EmailTagHelper class to the ASP.NET Core MVC project, enabling the use of a custom <kontakt> tag in Razor views. This tag helper converts the <kontakt> tag into an anchor (<a>) tag with a mailto: link. The DisplayName and Adresse properties set the display text and email address of the link, respectively. Updated _Layout.cshtml to use the new <kontakt> tag for contact information in the footer.

---
+++ SchulungQotd.Mvc/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SchulungQotd.Mvc.TagHelpers
+{
+    //Standard => <email>
+    //Custom => <kontakt>
+    [HtmlTargetElement("kontakt")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string DisplayName { get; set; }
+        public string Adresse { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; // <a...         
+
+            var href = !string.IsNullOrEmpty(Adresse)
+                ? $"mailto:{Adresse}"
+                : "";
+            output.Attributes.SetAttribute("href", href);
+            output.Content.SetContent(DisplayName);
+        }
+    }
+}
