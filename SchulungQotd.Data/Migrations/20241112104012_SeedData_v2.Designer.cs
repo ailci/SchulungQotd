e2697b0074e9d29d77e0942436ea34925c75e5aa
ailci <ali.ilci@gmx.de>
2024-11-12 11:42:38 +0100

    Add seed data for Quotes table
    Added initial seed data to the Quotes table in the QotdContextModelSnapshot.cs file. This includes multiple predefined quotes, each with a unique Id, an AuthorId to reference the author, and the QuoteText containing the quote itself. This ensures the Quotes table is populated with initial data when the database is created or updated.

--- SchulungQotd.Data/Migrations/20241112104012_SeedData_v2.Designer.cs
+++ SchulungQotd.Data/Migrations/20241112104012_SeedData_v2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchulungQotd.Data.Context;
 
@@ -11,9 +12,11 @@ using SchulungQotd.Data.Context;
 namespace SchulungQotd.Data.Migrations
 {
     [DbContext(typeof(QotdContext))]
-    partial class QotdContextModelSnapshot : ModelSnapshot
+    [Migration("20241112104012_SeedData_v2")]
+    partial class SeedData_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
