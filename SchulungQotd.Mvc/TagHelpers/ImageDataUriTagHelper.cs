140a7a1ce7f40383b71a43328b25013cd76cf1c0
ailci <ali.ilci@gmx.de>
2024-11-12 15:58:12 +0100

    image

---
+++ SchulungQotd.Mvc/TagHelpers/ImageDataUriTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SchulungQotd.Mvc.TagHelpers
+{
+    public class ImageDataUriTagHelper : TagHelper
+    {
+        public string? ImageMimeType { get; set; }
+        public byte[]? Image { get; set; }
+        public string Width { get; set; } = "120";
+        public string AltSrc { get; set; } = "https://via.placeholder.com/150";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+        }
+    }
+}
