433e5fde2a0abeb7e36ef02c6f436adb117a9c0f
ailci <ali.ilci@gmx.de>
2024-11-14 12:27:23 +0100

    Start Api

---
+++ SchulungQotd.WebApi/Controllers/AuthorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchulungQotd.WebApi.Controllers
+{
+    [Route("api/[controller]")]  // localhost:1234/api/authors
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        [HttpGet]  // localhost:1234/api/authors
+        public IActionResult GetQuoteOfTheDayAsync()
+        {
+            return Ok("hallo api");
+        }
+    }
+}
