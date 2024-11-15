e1968b3a0cb9a15c043c32a42d77779689a9e0ff
ailci <ali.ilci@gmx.de>
2024-11-15 13:31:53 +0100

    Persis

--- SchulungQotd.BlazorServer/Components/Pages/Home.razor.cs
+++ SchulungQotd.BlazorServer/Components/Pages/Home.razor.cs
@@ -3,22 +3,40 @@ using SchulungQotd.Service;
 using SchulungQotd.Shared.Models;
 
 namespace SchulungQotd.BlazorServer.Components.Pages;
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject] public IQotdService QotdService { get; set; } = default!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = default!;
+    private PersistingComponentStateSubscription _persistingComponentStateSubscription;
 
     protected override async Task OnInitializedAsync()
     {
+        _persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistData);
+
+        var statedLoaded = ApplicationState.TryTakeFromJson<QuoteOfTheDayViewModel>("qotd", out var qotd);
+        QotdViewModel = statedLoaded && qotd is not null ? qotd : await QotdService.GetQuoteOfTheDayAsync();
+
         //QotdViewModel = await QotdService.GetQuoteOfTheDayAsync();
     }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
+    private Task PersistData()
     {
-        if (firstRender)
-        {
-            QotdViewModel = await QotdService.GetQuoteOfTheDayAsync();
-            StateHasChanged();
-        }
+        ApplicationState.PersistAsJson("qotd", QotdViewModel);
+        return Task.CompletedTask;
+    }
+
+    //protected override async Task OnAfterRenderAsync(bool firstRender)
+    //{
+    //    if (firstRender)
+    //    {
+    //        QotdViewModel = await QotdService.GetQuoteOfTheDayAsync();
+    //        StateHasChanged();
+    //    }
+    //}
+
+    public void Dispose()
+    {
+        _persistingComponentStateSubscription.Dispose();
     }
 }
